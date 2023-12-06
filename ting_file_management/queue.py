3e0eddab4b6ce6ebc59fceebe10bff72c035f8e9
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-12-06 20:45:18 +0000

    feat: requisito 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,21 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.items = []
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.items)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.items.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.items) == 0:
+            return None
+        return self.items.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index >= 0 and index < len(self.items):
+            return self.items[index]
+        else:
+            raise IndexError("Índice Inválido ou Inexistente")
