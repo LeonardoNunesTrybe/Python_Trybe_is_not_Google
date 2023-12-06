6fb19e8ca43dc97b0e13bef1130cb0a24eb285b3
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-12-06 21:10:13 +0000

    feat: requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith('.txt'):
+            raise ValueError('Formato inválido')
+        with open(path_file, 'r', encoding='utf-8') as file:
+            return file.read().split('\n')
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        return None
+    except ValueError as error:
+        print(f"{error}", file=sys.stderr)
+        return None
