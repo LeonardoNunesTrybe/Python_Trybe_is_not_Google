9d1cbd5bbc740f367ab24c50e20a5d4ebb9f6917
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-12-07 19:53:41 +0000

    feat: requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for file in instance.items:
+        if file['nome_do_arquivo'] == path_file:
+            return None
+        
+    dict = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(txt_importer(path_file)),
+        'linhas_do_arquivo': txt_importer(path_file)
+    }
+
+    instance.enqueue(dict)
+    print(dict)
+    
 
 
 def remove(instance):
