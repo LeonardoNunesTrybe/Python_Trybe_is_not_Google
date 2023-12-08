426ee32c91314c344a1fce584fc7951aed85cd42
Leonardo Nunes <leonardonunes.uberbraga@gmail.com>
2023-12-08 00:21:25 +0000

    feat: requisito 8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -22,4 +22,26 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+
+    # Itera sobre cada arquivo na fila
+    for file in instance.items:
+        occ = []
+
+        for line_number, line in enumerate(file["linhas_do_arquivo"], start=1):
+            # Verifica se a palavra (case insensitive) está na linha
+            if word.lower() in line.lower():
+                occ.append({
+                    'linha': line_number,
+                    'conteudo': line
+                })
+
+        # Se a palavra foi encontrada, adiciona as informações à lista
+        if occ:
+            result.append({
+                'palavra': word,
+                'arquivo': file['nome_do_arquivo'],
+                'ocorrencias': occ
+            })
+
+    return result
